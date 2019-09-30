03dc718c9fcd28e71b9780669be7d37168a176e8
Douglas Siqueira Gonçalves <ds417042@gmail.com>
2019-09-29 21:13:32 -0300

    Update enviar.py

--- enviar.py
+++ enviar.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import pika
+import pika, os
 import json
 import base64
 
@@ -9,13 +9,16 @@ with open('file.json') as json_file:
 message = json.dumps(data)
 
 
-credentials = pika.PlainCredentials('dodo', 'dodo123.') 
-parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-channel.queue_declare(queue='hello')
+# Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
+url = os.environ.get('popzhidh', 'amqp://popzhidh:SUZJ6DD-2XCF2ms9b35bv3hgIqMOXyB4@spider.rmq.cloudamqp.com/popzhidh')
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='hello') # Declare a queue
 channel.basic_publish(exchange='',
                       routing_key='hello',
                       body=message)
+
 print(" [x] Sent 'Hello World!'")
 connection.close()
+
