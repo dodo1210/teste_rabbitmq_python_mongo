a72a86664387e3d1410338b9ce29e35d91b51d82
Douglas Siqueira Gonçalves <ds417042@gmail.com>
2019-09-29 21:13:55 -0300

    Update receber.py

--- receber.py
+++ receber.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
-import pika
+import pika, os
 import json
 from pymongo import MongoClient 
 import base64
 
-credentials = pika.PlainCredentials('dodo', 'dodo123.') 
-parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-channel.queue_declare(queue='hello')
-
+# Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
+url = os.environ.get('popzhidh', 'amqp://popzhidh:SUZJ6DD-2XCF2ms9b35bv3hgIqMOXyB4@spider.rmq.cloudamqp.com/popzhidh')
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel() # start a channel
+channel.queue_declare(queue='hello') # Declare a queue
 def callback(ch, method, properties, body):
-    
+    print(" [x] Received " + str(body))
+    data = []
     data = json.loads(body)
     print(body)
 
@@ -19,7 +20,16 @@ def callback(ch, method, properties, body):
     mydatabase = client['teste']
     mycol = mydatabase['col']
     mycol.insert(data)
-    
-channel.basic_consume(callback,queue='hello',no_ack=True)
-print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+
+
+print(' [*] Waiting for messages:')
 channel.start_consuming()
+connection.close()
+
+
