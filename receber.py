4be3d7737e1d541d4a9a5c0ff39db25b463e39e2
Douglas Siqueira Gonçalves <ds417042@gmail.com>
2019-10-01 00:35:26 -0300

    Update receber.py

--- receber.py
+++ receber.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import pika, os
-import json
 from pymongo import MongoClient 
 import base64
+import ast
 
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
 url = os.environ.get('popzhidh', 'amqp://popzhidh:SUZJ6DD-2XCF2ms9b35bv3hgIqMOXyB4@spider.rmq.cloudamqp.com/popzhidh')
@@ -12,16 +12,12 @@ channel = connection.channel() # start a channel
 channel.queue_declare(queue='hello') # Declare a queue
 def callback(ch, method, properties, body):
     print(" [x] Received " + str(body))
-    data = []
-    data = json.loads(body)
     print(body)
 
     client = MongoClient("mongodb://localhost:27017/")
     mydatabase = client['teste']
     mycol = mydatabase['col']
-    mycol.insert(data)
-
-
+    mycol.insert(ast.literal_eval(body))
 
 channel.basic_consume(callback,
                       queue='hello',
@@ -31,5 +27,3 @@ channel.basic_consume(callback,
 print(' [*] Waiting for messages:')
 channel.start_consuming()
 connection.close()
-
-
