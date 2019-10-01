9c59853c4b4a0abf7fd7bfa82a9ca3766a8b61df
Douglas Siqueira Gonçalves <ds417042@gmail.com>
2019-10-01 00:53:33 -0300

    Update enviar.py

--- enviar.py
+++ enviar.py
@@ -4,8 +4,7 @@ import json
 import base64
 import urllib
 
-response = urllib.urlopen("file.json")
-data = json.dumps(response.read())
+response = urllib.urlopen("file.json").read()
 
 # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
 url = os.environ.get('popzhidh', 'amqp://popzhidh:SUZJ6DD-2XCF2ms9b35bv3hgIqMOXyB4@spider.rmq.cloudamqp.com/popzhidh')
@@ -15,7 +14,7 @@ channel = connection.channel() # start a channel
 channel.queue_declare(queue='hello') # Declare a queue
 channel.basic_publish(exchange='',
                       routing_key='hello',
-                      body=data)
+                      body=str(json.loads(response)))
 
 print(" [x] Sent 'Hello World!'")
 connection.close()
