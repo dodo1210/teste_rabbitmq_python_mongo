60bccfc86e7146ed94a3ff8e78cf29eb1e95ceaf
Douglas Siqueira Gonçalves <ds417042@gmail.com>
2019-09-30 23:43:21 -0300

    Update mostrar.py

--- mostrar.py
+++ mostrar.py
@@ -1,8 +1,19 @@
 from pymongo import MongoClient 
+import pprint
 
+pp = pprint.PrettyPrinter(indent=4)
 client = MongoClient("mongodb://localhost:27017/")
 mydatabase = client['teste']
 mycol = mydatabase['col']
 
-for x in mycol.find({},{ "user_id": 0 }):
-  print(x)
+cont = 0
+#apresenta valores
+for x in mycol.find():
+  #apresenta todos
+  pp.pprint(x)
+  if x["request_method"] == "POST":
+    cont+=1
+  #apresenta apenas id
+  pp.pprint(x["_id"])
+#resultado da quantidade de resquisições do método POST
+print(cont)
